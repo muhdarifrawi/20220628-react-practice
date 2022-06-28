1abd3fb1b170bc82dcd90e004c4684807b613ffd
arif <muhdarifrawi@gmail.com>
2022-06-28 15:48:46 +0800

    Added radio button: - Two-way binding is working.

--- form-processing/src/components/ExampleForm.js
+++ form-processing/src/components/ExampleForm.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default class ExampleForm extends React.Component{
     state={
-        username:"yosemite sam"
+        username:"yosemite sam",
+        fruits:""
     }
 
     updateUsername = (event) =>{
@@ -11,21 +12,60 @@ export default class ExampleForm extends React.Component{
         })
     }
 
+    updateFruits = (event) => {
+        this.setState({
+            fruits:event.target.value
+        })
+    }
+
     render(){
         return (
             <React.Fragment>
                 <h1>Example Form:</h1>
                 <div>
-                    <label for="username" class="form-label">
+                    <label for="username" className="form-label">
                         Username:
                     </label>
-                    <input type="text" id="username"
+                    <input type="text" name="username"
                         className="form-control"
                         value={this.state.username}
                         onChange={this.updateUsername}
                     >
-
                     </input>
+                    <h3>Select Fruits</h3>
+                    <div className="form-check">
+                        <input type="radio" name="fruits"
+                            className="form-check-input"
+                            value="apple"
+                            onChange={this.updateFruits}
+                            checked={this.state.fruits === "apple"}
+                        ></input>
+                        <label for="fruits" className="form-check-label">
+                            Apple   
+                        </label>
+                    </div>
+                    <div className="form-check">    
+                        <input type="radio" name="fruits"
+                            className="form-check-input"
+                            value="banana"
+                            onChange={this.updateFruits}
+                            checked={this.state.fruits === "banana"}
+                        ></input>
+                        <label for="fruits" className="form-check-label">
+                            Banana
+                        </label>
+                    </div>
+                    <div className="form-check">    
+                        <input type="radio" name="fruits"
+                            className="form-check-input"
+                            value="cherry"
+                            onChange={this.updateFruits}
+                            checked={this.state.fruits === "cherry"}
+                        ></input>
+                        <label for="fruits" className="form-check-label">
+                            Cherry
+                        </label>
+                    </div>
                 </div>
             </React.Fragment>
             
