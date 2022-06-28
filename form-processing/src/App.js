19f8f3ca7a4eb8b0751cc0fa26ac92f0bd574800
arif <muhdarifrawi@gmail.com>
2022-06-28 14:12:36 +0800

    Created username input - Created two-way binding. - Can be edited through typing or change of state. - Working.

--- form-processing/src/App.js
+++ form-processing/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.esm"
 import './App.css';
 import ExampleForm from './components/ExampleForm';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <ExampleForm/>
     </div>
   );
--- form-processing/src/components/ExampleForm.js
+++ form-processing/src/components/ExampleForm.js
@@ -1,9 +1,34 @@
 import React from "react"
 
 export default class ExampleForm extends React.Component{
+    state={
+        username:"yosemite sam"
+    }
+
+    updateUsername = (event) =>{
+        this.setState({
+            username:event.target.value
+        })
+    }
+
     render(){
         return (
-            <h1>Example Form:</h1>
+            <React.Fragment>
+                <h1>Example Form:</h1>
+                <div>
+                    <label for="username" class="form-label">
+                        Username:
+                    </label>
+                    <input type="text" id="username"
+                        className="form-control"
+                        value={this.state.username}
+                        onChange={this.updateUsername}
+                    >
+
+                    </input>
+                </div>
+            </React.Fragment>
+            
         )
     }
 }
